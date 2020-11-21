d8b4cc0b0c9d16b1252f822b6b66fd855b96c5af
yangshengkai666 <1151278839@qq.com@example.com>
2020-11-22 00:32:23 +0800

    ychange

--- sw/src/main/java/com/yang/ysk/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.yang.ysk.dao;
-
-import com.yang.ysk.entity.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserDAO {
-    User login(@Param("username") String username,@Param("password") String password);
-}
